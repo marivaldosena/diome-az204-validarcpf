6fe817d6a9a3fde2419a21f314d48a0bfa5d6a56
Marivaldo Sena <marivaldosena@users.noreply.github.com>
2025-01-16 21:56:14 -0300

    Criar método para validar CPF.

--- ValidadorCpf.cs
+++ ValidadorCpf.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -15,10 +17,90 @@ namespace DiomeValidacoes.Documentos
         }
 
         [Function("validarcpf")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("Iniciando a validação de CPF.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            Dictionary<string?, string?> data;
+            try
+            {
+                data = JsonSerializer.Deserialize<Dictionary<string?, string?>>(requestBody);
+            }
+            catch (JsonException)
+            {
+                return new BadRequestObjectResult(new { message = "Erro ao desserializar o JSON." });
+            }
+
+            if (data == null || !data.ContainsKey("cpf"))
+            {
+                return new BadRequestObjectResult(new { message = "O campo 'cpf' é obrigatório." });
+            }
+
+            string? cpf = data["cpf"];
+
+            if (string.IsNullOrWhiteSpace(cpf)) {
+                return new BadRequestObjectResult(new { message = "O campo 'cpf' não pode ser nulo, vazio ou espaços em branco." });
+            }
+
+            if (!IsValidCpf(cpf))
+            {
+                return new BadRequestObjectResult(new { message = "CPF inválido." });
+            }
+
+            return new OkObjectResult("CPF é válido.");
+        }
+
+        internal static bool IsValidCpf(string cpf)
+        {
+            if (cpf.Length != 11 || !long.TryParse(cpf, out _))
+            {
+                return false;
+            }
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            }
+
+            int resto = soma % 11;
+            if (resto < 2)
+            {
+                resto = 0;
+            }
+            else
+            {
+                resto = 11 - resto;
+            }
+
+            string digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            }
+
+            resto = soma % 11;
+            if (resto < 2)
+            {
+                resto = 0;
+            }
+            else
+            {
+                resto = 11 - resto;
+            }
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
         }
     }
 }
